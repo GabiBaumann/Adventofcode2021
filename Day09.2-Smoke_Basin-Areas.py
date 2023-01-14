c468ecea02a1d7200698180f112cd0677418447d
Andre Müller <andre.mueller@gesis.org>
2023-01-14 22:29:47 +0100

    Day09.2: Solution
    Changes to be committed:
    	modified:   Day09.2-Smoke_Basin-Areas.py

--- Day09.2-Smoke_Basin-Areas.py
+++ Day09.2-Smoke_Basin-Areas.py
@@ -81,9 +81,23 @@ count all overwrites until meeting max(x) and 9...
 Actually, do recursion to keep track of odd shapes? 
 """
 
+def checkbasin(y, x):
+    r = 1
+    grid[y][x] = 9
+    if grid[y][x-1] < 9:
+        r += checkbasin(y, x-1)
+    if grid[y-1][x] < 9:
+        r += checkbasin(y-1, x)
+    if grid[y][x+1] < 9:
+        r+= checkbasin(y, x+1)
+    if grid[y+1][x] < 9:
+        r += checkbasin(y+1, x)
+    return r
+
+
 grid = []
 padding = []
-risk = 0
+basins = []
 
 #with open('Day09-Input--Debug', 'r') as file:
 with open('Day09-Input', 'r') as file:
@@ -101,7 +115,20 @@ grid.append(padding)
 
 for y in range(1, len(grid)):
     for x in range(1, len(row)):
-        if grid[y][x-1] > grid[y][x] < grid[y][x+1] and grid[y-1][x] > grid[y][x] < grid[y+1][x]:
-            risk += grid[y][x] + 1
+        size = 0
+        if grid[y][x] < 9:
+            size = checkbasin(y, x)
+            print(size)
+        if size:
+            basins.append(size)
+            print(basins)
 
-print(risk)
+basins = sorted(basins)
+print(basins)
+
+size = 1
+for i in range(3):
+    size *= basins.pop()
+print(size)
+
+# 1113424
